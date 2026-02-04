1e5c81d382a2ed3baaa6305481bf15658271a502
ragnar <ranelt@taltech.ee>
2026-02-04 21:17:30 +0200

    maybe will get gh-pages to work

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '//', // <-- Add your GitHub Pages repo name here
+  base: '/', // <-- Add your GitHub Pages repo name here
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
